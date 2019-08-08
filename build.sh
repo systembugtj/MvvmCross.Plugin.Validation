d08b1087ddb488cd313d0242faf9f1aa52ed7ad8
systembug <albert_lee@hotmail.com>
2019-08-08 13:38:10 -0700

    6.x clean up

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
+rm -rf src/bin
+rm *.nupkg
+
 nuget restore src/MvvmCross.Plugin.Validation.sln -verbosity detailed
 msbuild src/MvvmCross.Plugin.Validation.sln /p:Configuration=Release
 
 nuget pack nuspec/MVVMCross.Plugin.Validation.nuspec
 nuget pack nuspec/MVVMCross.Plugin.Validation.ForFieldBinding.nuspec
-# nuget add Redux.Net.3.0.0.nupkg -source ~/.nuget/package
-
-
-# nuget restore examples/todomvc/Redux.TodoMvc.sln -verbosity detailed
-# msbuild examples/todomvc/Redux.TodoMvc.sln /p:Configuration=Release
